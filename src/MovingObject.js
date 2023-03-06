09baa150a477a0e66a76232962a58a4d9e07a3cc
useNavigate <useNavigate@gmail.com>
2023-03-06 17:04:32 -0500

    Q_Q

--- src/MovingObject.js
+++ src/MovingObject.js
@@ -1,10 +1,15 @@
 class MovingObject {
   constructor(args) {
-
-    this.pos = args.pos
+    this.pos = args.pos;
     this.vel = args.vel;
     this.radius = args.radius;
     this.color = args.color;
+  }
+
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.arc(this.centerX, this.centerY, this.radius, 0, 2 * Math.PI, false);
+    ctx.fillStyle = this.color;
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import MovingObject from "./MovingObject.js";
 
 // const object = require("./MovingObject.js")
 
+
 let test = new MovingObject({
   pos: [1, 2],
   vel: 10,
   radius: 5,
   color: "#00FF00",
 });
+
+const canvas = document.getElementById("game-canvas");
+const ctx = canvas.getContext("2d");
+test.draw(ctx)
+
 window.MovingObject = MovingObject;
 window.test = test;
