2147639259cf3c0c524481a4916a74e036dbb8dc
Kaiter Wu <wukaiter@gmail.com>
2023-03-06 17:24:14 -0500

    drawing a circle

--- src/index.js
+++ src/index.js
@@ -6,15 +6,19 @@ import MovingObject from "./MovingObject.js";
 
 
 let test = new MovingObject({
-  pos: [1, 2],
+  pos: [100,200],
   vel: 10,
-  radius: 5,
+  radius: 50,
   color: "#00FF00",
 });
 
+
 const canvas = document.getElementById("game-canvas");
 const ctx = canvas.getContext("2d");
-test.draw(ctx)
-
+// debugger
+// test.draw(ctx)
+window.canvas = canvas
+window.ctx = ctx
 window.MovingObject = MovingObject;
 window.test = test;
+// window.circle = test.draw(ctx)
